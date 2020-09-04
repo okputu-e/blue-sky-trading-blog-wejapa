8b04b1558e18e2079c2d8af804738ebf3ac5f265
immanuelredd <66283891+immanuelredd@users.noreply.github.com>
2020-09-04 15:32:00 +0100

    created landing page
    Added style and markup!

---
+++ includes/hero.php
@@ -0,0 +1,20 @@
+<div class="hero">
+    <div class="welcome_msg">
+        <h1>Today's Inspiration</h1>
+        <p>
+            We at BlueSky,<br>
+            we don't catch feelings. <br>
+            We catch pips. <br>
+            <span>~ Jaystan Okputu</span>
+        </p>
+        <a href="register.php" class="btn">Join us!</a>
+    </div>
+    <div class="login_div">
+        <form action="index.php" method="post">
+            <h2>Login</h2>
+            <input type="text" name="username" placeholder="Username" autocomplete="off">
+            <input type="password" name="password" placeholder="Password">
+            <button class="btn" type="submit" name="login_btn">Sign in</button>
+        </form>
+    </div>
+</div>
