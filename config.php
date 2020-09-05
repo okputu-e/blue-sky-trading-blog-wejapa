2753ece96270f96f3d2f90984b596a5f84851ec2
immanuelredd <66283891+immanuelredd@users.noreply.github.com>
2020-09-05 23:13:32 +0100

    heroku details

--- config.php
+++ config.php
@@ -1,14 +1,14 @@
 <?php
 
     session_start();
-    $conn = mysqli_connect("localhost", "root", "", "blue-sky");
+    $conn = mysqli_connect("us-cdbr-east-02.cleardb.com", "bf75824285893b", "2e9df13d", "heroku_66313510d928b51");
 
     if (!$conn) {
         die('Error encounter while connecting to Database' . mysqli_connect_error());
     }
 
     define('ROOT_PATH', realpath(dirname(__FILE__)));
-    define('BASE_URL', 'http://localhost/blue-sky-trading-blog-wejapa');
+    define('BASE_URL', 'http://us-cdbr-east-02.cleardb.com/blue-sky-trading-blog-wejapa');
     
 
 
