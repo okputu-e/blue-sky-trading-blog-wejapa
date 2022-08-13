61cf2ad64e06bfd5dd25c677adc47e1e4e391020
okputu-e <immanuelokputu94@gmail.com>
2022-08-13 23:11:00 +0100

    first commit

--- config.php
+++ config.php
@@ -1,19 +1,16 @@
 <?php
 
-   session_start();
-  //$conn = mysqli_connect("db4free.net","great_blue_sky94","great_blue_sky94", "great_blue_sky94");
+session_start();
+//$conn = mysqli_connect("db4free.net","great_blue_sky94","great_blue_sky94", "great_blue_sky94");
 
-  //$conn = mysqli_connect("localhost","root","", "blue-sky");
+$conn = mysqli_connect("localhost", "root", "", "blue-sky");
 
-  $conn = mysqli_connect("remotemysql.com","DaQIGilviO","tz3KhX2HPY", "DaQIGilviO");
+//   $conn = mysqli_connect("remotemysql.com","DaQIGilviO","tz3KhX2HPY", "DaQIGilviO");
 
-  if (!$conn) {
-      die("Error connecting to Database" . mysqli_connect_error());
-  }
- 
-    define('ROOT_PATH', realpath(dirname(__FILE__)));
-    define('BASE_URL', 'https://bluesky-blog-wejapa.herokuapp.com');
-    //define('BASE_URL', 'http://localhost/blue-sky-trading-blog-wejapa');
-    
+if (!$conn) {
+    die("Error connecting to Database" . mysqli_connect_error());
+}
 
-?>
+define('ROOT_PATH', realpath(dirname(__FILE__)));
+// define('BASE_URL', 'https://bluesky-blog-wejapa.herokuapp.com');
+define('BASE_URL', 'http://localhost/blue-sky-trading-blog-wejapa');
